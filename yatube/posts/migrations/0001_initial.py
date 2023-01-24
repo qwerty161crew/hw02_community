302750b04cef626aa90ce29f752c55816bda0875
Алексей Матвеев <qwerty161crew@gmail.com>
2023-01-24 21:42:39 +0300

    ошибок нет

--- yatube/posts/migrations/0001_initial.py
+++ yatube/posts/migrations/0001_initial.py
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
                 ('text', models.TextField()),
                 ('pub_date', models.DateTimeField(auto_now_add=True)),
                 ('author', models.ForeignKey(on_delete=django.db.models.
-                                             deletion.CASCADE, 
+                                             deletion.CASCADE,
                                              related_name='posts',
                                              to=settings.AUTH_USER_MODEL)),
             ],
