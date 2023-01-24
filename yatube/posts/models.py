69573b4748061f7e657e82dc14fbc00ab83204ac
Алексей Матвеев <qwerty161crew@gmail.com>
2023-01-24 21:05:27 +0300

    ревью

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
@@ -8,8 +8,10 @@ class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
+
     def __str__(self):
         return self.title
+
 
 class Post(models.Model):
     text = models.TextField()
