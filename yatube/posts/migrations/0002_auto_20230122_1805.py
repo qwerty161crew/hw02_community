8409f6e2d4009039248ebd4caddc8e9e4e7fdb02
Алексей Матвеев <qwerty161crew@gmail.com>
2023-01-24 21:34:40 +0300

    доделывал дизайн кода

--- yatube/posts/migrations/0002_auto_20230122_1805.py
+++ yatube/posts/migrations/0002_auto_20230122_1805.py
@@ -14,7 +14,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Group',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True,
+                 serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=200)),
                 ('slug', models.SlugField(unique=True)),
                 ('description', models.TextField()),
@@ -23,6 +24,9 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='post',
             name='group',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='posts.Group'),
+            field=models.ForeignKey(blank=True, null=True,
+                                    on_delete=django.db.models.deletion
+                                    .CASCADE,
+                                    to='posts.Group'),
         ),
     ]
