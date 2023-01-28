8d02557b9fbe0f281a5b97589033108b1e340d9e
Алексей Матвеев <qwerty161crew@gmail.com>
2023-01-28 20:10:12 +0300

    добавил систему авторизации

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts.apps.PostsConfig'
+    'posts.apps.PostsConfig',
+    'users.apps.UsersConfig',
+    'core.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
