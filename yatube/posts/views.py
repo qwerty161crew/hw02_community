7cfd7815335568b661c84e7db35bb487ad38057b
Алексей Матвеев <qwerty161crew@gmail.com>
2023-02-04 10:25:59 +0300

    сдал 3 проект и приступаю к четвертому

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,19 +1,24 @@
+from django.core.paginator import Paginator
+
 from django.shortcuts import get_object_or_404, render
 # Create your views here.
 from .models import Post, Group
 
 
 def index(request):
-    posts = Post.objects.all()[:10]
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts
+        'page_obj': page_obj
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()
+    posts = Post.objects.filter(group=group).all()[:10]
     context = {
         'group': group,
         'posts': posts,
